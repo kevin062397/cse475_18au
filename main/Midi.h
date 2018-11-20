93e7047c5a1bc8c73292efb00377aedc1d93544f
Haoyuan Xia <41046146+kevin062397@users.noreply.github.com>
2018-11-20 02:43:39 -0800

    Added custom sound gestures

--- main/Midi.h
+++ main/Midi.h
@@ -20,7 +20,7 @@
 #define VS1053_GM1_SLAP_BASS_1 36
 #define VS1053_GM1_CELLO 43
 
-#define MIDI_NOTE_ON  0x90
+#define MIDI_NOTE_ON 0x90
 #define MIDI_NOTE_OFF 0x80
 #define MIDI_CHAN_MSG 0xB0
 #define MIDI_CHAN_BANK 0x00
@@ -33,55 +33,63 @@
 void tcReset();
 bool tcIsSyncing();
 
-class Midi {
- public:
-  // More to come, add your sounds here!
-  static Sound scale;
-  static Sound bbwa;
-  static Sound junco;
-  static Sound cardinal;
-  static Sound bpwa;
-  static Sound brcr;
-  static Sound oriole;
-  static Sound osprey;
-  static Sound oven;
-  static Sound songspar;
-  static Sound towhee;
-  static Sound tuftedti;
-  static Sound veery;
-  static Sound whthsprw;
-  static Sound indigobu;
-  static Sound mowa;
-  static Sound wiwa;
+class Midi
+{
+  public:
+	// More to come, add your sounds here!
+	static Sound scale;
+	static Sound bbwa;
+	static Sound junco;
+	static Sound cardinal;
+	static Sound bpwa;
+	static Sound brcr;
+	static Sound oriole;
+	static Sound osprey;
+	static Sound oven;
+	static Sound songspar;
+	static Sound towhee;
+	static Sound tuftedti;
+	static Sound veery;
+	static Sound whthsprw;
+	static Sound indigobu;
+	static Sound mowa;
+	static Sound wiwa;
 
-  /** Array of all sounds, in a fixed order, used to assign an index to each. Add your sounds to this! */
-  static constexpr Sound *SOUNDS[] =
-      {nullptr, &Midi::scale, &Midi::bbwa, &Midi::bpwa, &Midi::brcr, &Midi::cardinal, &Midi::indigobu, &Midi::junco,
-       &Midi::mowa, &Midi::oriole, &Midi::osprey, &Midi::oven, &Midi::songspar, &Midi::towhee, &Midi::tuftedti,
-       &Midi::veery, &Midi::whthsprw, &Midi::wiwa};
+	static Sound startle;
+	static Sound ambient1;
+	static Sound ambient2;
+	static Sound ambient3;
+	static Sound active1;
+	static Sound active2;
+	static Sound active3;
 
-  /**
-   * Use this to set the current sound gesture.
-   *
-   * @param soundIdx Index of new sound to play in Midi::SOUNDS.
-   */
-  static void setSound(uint8_t soundIdx);
+	/** Array of all sounds, in a fixed order, used to assign an index to each. Add your sounds to this! */
+	static constexpr Sound *SOUNDS[] =
+		{nullptr, &Midi::scale, &Midi::bbwa, &Midi::bpwa, &Midi::brcr, &Midi::cardinal, &Midi::indigobu, &Midi::junco, &Midi::mowa, &Midi::oriole, &Midi::osprey, &Midi::oven, &Midi::songspar, &Midi::towhee, &Midi::tuftedti, &Midi::veery, &Midi::whthsprw, &Midi::wiwa, &Midi::startle, &Midi::ambient1, &Midi::ambient2, &Midi::ambient3, &Midi::active1, &Midi::active2, &Midi::active3};
 
-  /**
-   * @returns the index of the current sound gesture.
-   */
-  static uint8_t getSound();
+	/**
+	 * Use this to set the current sound gesture.
+	 *
+	 * @param soundIdx Index of new sound to play in Midi::SOUNDS.
+	 */
+	static void setSound(uint8_t soundIdx);
 
-  static void setup();
+	/**
+	 * @returns the index of the current sound gesture.
+	 */
+	static uint8_t getSound();
 
-  /** Current sound being played. */
-  static Sound *_current;
- private:
-  /** Index of current sound being played. */
-  static uint8_t _currentIdx;
+	static void setup();
 
-  static void tcStartCounter();
-  static void tcConfigure(int frequencyHz);
+	/** Current sound being played. */
+	static Sound *_current;
+
+  private:
+	/** Index of current sound being played. */
+	static uint8_t _currentIdx;
+
+	static void tcStartCounter();
+	static void tcConfigure(int frequencyHz);
 };
 
-#endif  // _MIDI_H_
+#endif // _MIDI_H_

a7f99cbcec4dd67f1db170a1fd7acd78af9f1d04
Haoyuan (Kevin) Xia <41046146+kevin062397@users.noreply.github.com>
2018-12-08 03:34:10 -0800

    Updated the `globalWeights`

--- main/State.h
+++ main/State.h
@@ -122,7 +122,7 @@ class State
 	/** Reference to the creature this is a state in */
 	Creature &_creature;
 
-	int8_t _globalWeights[ACTIVE_STATES + AMBIENT_STATES] = {1, 1, 1, 1, 1, 1};
+	int8_t _globalWeights[ACTIVE_STATES + AMBIENT_STATES] = {4, -2, 4, -2, 4, -2};
 
   private:
 	char _name[MAX_NAME_LEN + 1];
